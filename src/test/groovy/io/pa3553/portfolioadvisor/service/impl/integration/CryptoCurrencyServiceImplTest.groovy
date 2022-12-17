54047c717e90ecad56bba386d4b96c20271042d4
pa 3553 <luisilveira@gmail.com>
2022-12-17 20:48:18 +0000

    add test

---
+++ src/test/groovy/io/pa3553/portfolioadvisor/service/impl/integration/CryptoCurrencyServiceImplTest.groovy
@@ -0,0 +1,46 @@
+package io.pa3553.portfolioadvisor.service.impl.integration
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Specification
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CryptoCurrencyServiceImplTest extends Specification
+{
+    def "test createCryptoCurrency"() {
+//        given:
+//
+//        when:
+//        // TODO implement stimulus
+//        then:
+//        // TODO implement assertions
+    }
+
+    def "test findCryptoCurrency"() {
+//        given:
+//
+//        when:
+//        // TODO implement stimulus
+//        then:
+//        // TODO implement assertions
+    }
+
+    def "test testFindCryptoCurrency"() {
+//        given:
+//
+//        when:
+//        // TODO implement stimulus
+//        then:
+//        // TODO implement assertions
+    }
+
+    def "test removeCryptoCurrency"() {
+//        given:
+//
+//        when:
+//        // TODO implement stimulus
+//        then:
+//        // TODO implement assertions
+    }
+}
