11ac041e5c0336fe43720184d6496b41ffe5afd2
pa3553 <luisilveira@gmail.com>
2022-12-23 17:34:26 +0000

    #0 add tests

--- src/test/groovy/io/pa3553/portfolioadvisor/service/impl/integration/CryptoCurrencyServiceImplTest.groovy
+++ src/test/groovy/io/pa3553/portfolioadvisor/service/impl/integration/CryptoCurrencyServiceImplTest.groovy
@@ -1,5 +1,8 @@
 package io.pa3553.portfolioadvisor.service.impl.integration
 
+import io.pa3553.portfolioadvisor.model.CreateCryptoCurrencyContext
+import io.pa3553.portfolioadvisor.service.CryptoCurrencyService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import spock.lang.Specification
@@ -8,39 +11,77 @@ import spock.lang.Specification
 @ActiveProfiles("test")
 class CryptoCurrencyServiceImplTest extends Specification
 {
+    @Autowired
+    CryptoCurrencyService cryptoCurrencyService
+
+    def cleanup()
+    {
+//        wishListRepository.deleteAll()
+//        wishListCryptoCurrencyRepository.deleteAll()
+//        cryptoCurrencyRepository.deleteAll()
+    }
+
     def "test createCryptoCurrency"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
+        given:
+        def coinName = "Ethereum"
+        def coinSymbol = "ETH"
+        def context = new CreateCryptoCurrencyContext(coinName, coinSymbol)
+
+        when:
+        def cryptoCurrency = cryptoCurrencyService.createCryptoCurrency(context)
+
+        then:
+        cryptoCurrency
     }
 
     def "test findCryptoCurrency"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
+        given:
+        def coinName = "Ethereum"
+        def coinSymbol = "ETH"
+        def context = new CreateCryptoCurrencyContext(coinName, coinSymbol)
+
+        when:
+        def cryptoCurrency = cryptoCurrencyService.createCryptoCurrency(context)
+
+        then:
+        cryptoCurrency
+        def cryptoCurrencyDB = cryptoCurrencyService.findCryptoCurrency(cryptoCurrency.id)
+        cryptoCurrency == cryptoCurrencyDB
     }
 
     def "test testFindCryptoCurrency"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
+        given:
+        def coinName = "Ethereum"
+        def coinSymbol = "ETH"
+        def context = new CreateCryptoCurrencyContext(coinName, coinSymbol)
+
+        when:
+        def cryptoCurrency = cryptoCurrencyService.createCryptoCurrency(context)
+
+        then:
+        cryptoCurrency
+        def cryptoCurrencyDB = cryptoCurrencyService.findCryptoCurrency(coinSymbol)
+        cryptoCurrency == cryptoCurrencyDB
     }
 
     def "test removeCryptoCurrency"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
+        given:
+        def coinName = "Ethereum"
+        def coinSymbol = "ETH"
+        def context = new CreateCryptoCurrencyContext(coinName, coinSymbol)
+
+        when:
+        def cryptoCurrency = cryptoCurrencyService.createCryptoCurrency(context)
+
+        then:
+        cryptoCurrency
+        def cryptoCurrencyDB = cryptoCurrencyService.findCryptoCurrency(cryptoCurrency.id)
+        cryptoCurrency == cryptoCurrencyDB
+
+        when:
+        cryptoCurrencyService.removeCryptoCurrency(cryptoCurrency.id)
+
+        then:
+        !cryptoCurrencyService.findCryptoCurrency(cryptoCurrency.id)
     }
 }
